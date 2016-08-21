d94c8b8d0fe6dfab65f18bcedb6a38e86d971d57
Jerin Philip <monu1618@gmail.com>
2016-08-22 00:17:56 +0530

    WSME REST Capability
    setup.cfg, setup.py -> pbr enabled. Entry points added.
    katapayadi/expose.py -> WSME Functions
    katapayadi/__init__.py -> Relative import

--- setup.py
+++ setup.py
@@ -1,29 +1,8 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-
-name = "katapayadi"
+from setuptools import setup
 
 setup(
-    name=name,
-    version="0.2.0",
-    url = "http://silpa.org.in/Katapayadi",
-    license="LGPL-3.0",
-    description="Decode katapayadi number system",
-    author="Santhosh Thottingal",
-    author_email="santhosh.thottingal@gmail.com",
-    long_description="""
-    Katapayadi sankhya is a simplification of Aryabhata's
-    Sanskrit numerals, due probably to Haridatta from Kerala.
-    In Malayalam it is also known as 'Paralperu'
-    For eg: ചണ്ഡാംശുചന്ദ്രാധമകുംഭിപാല represents 31415926536 which
-    is π*1000000000000000
-    More examples in Malayalam are given in this page
-    """,
-    packages=find_packages(),
-    include_package_data=True,
-    setup_requires=['setuptools-git'],
-    install_requires=['setuptools','indicsyllabifier','silpa_common'],
-    test_suite='tests',
-    zip_safe=False,
+    setup_requires=['pbr'],
+    pbr=True
 )
+
