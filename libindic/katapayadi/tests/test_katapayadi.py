57431cb17b22b3a8e906e1e636cf34d19cbb5a7e
Balasankar C <balasankarc@autistici.org>
2017-09-02 19:24:33 +0530

    Add more test strings

--- libindic/katapayadi/tests/test_katapayadi.py
+++ libindic/katapayadi/tests/test_katapayadi.py
@@ -13,7 +13,14 @@ class KatapayadiTest(TestCase):
         self.katapayadi = Katapayadi()
 
     def test_katapayadi(self):
-        self.assertEqual(self.katapayadi.get_number(u"കമല"),351)
+        self.assertEqual(self.katapayadi.get_number(u"കമല"), 351)
+        self.assertEqual(self.katapayadi.get_number(u"വ്യാജം"), 81)
+        self.assertEqual(self.katapayadi.get_number(u"സത്യം"), 17)
+        self.assertEqual(self.katapayadi.get_number(u"കൃഷ്ണ"), 51)
+        self.assertEqual(self.katapayadi.get_number(u"ലീല"), 33)
+        self.assertEqual(self.katapayadi.get_number(u"മനസ്സു്"), 705)
+        self.assertEqual(self.katapayadi.get_number(u"കാമം"), 51)
+        self.assertEqual(self.katapayadi.get_number(u"മോക്ഷം"), 65)
 
     def test_swarasthana(self):
-        self.assertEqual(self.katapayadi.get_swarasthanas(1)[2],"Ga1")
+        self.assertEqual(self.katapayadi.get_swarasthanas(1)[2], "Ga1")
